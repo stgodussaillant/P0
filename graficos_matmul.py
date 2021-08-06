9a1f98bda7fc69fe80631dbbb4649e3dee58c9e6
stgodussaillant <88340864+stgodussaillant@users.noreply.github.com>
2021-08-05 23:21:04 -0400

    Add files via upload

---
+++ graficos_matmul.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+f = open("Rendimiento.txt", "r")
+
+datos = f.read()
+datos = datos.split("\n")
+datos.pop(0)
+datos.pop(-1)
+q=0
+tiempo = []
+tamano = []
+memoria = []
+while q<len(datos):
+    fila = datos[q]
+    fila = fila.split("   ")
+    tiempo.append(float(fila[0]))
+    memoria.append(float(fila[1]))
+    tamano .append(float(fila[2]))
+    q+=1
+
+N = len(datos)/10
+N = int(N)
+
+x1 = ["10","20","50","100","200","500","1000","2000","5000","10000","20000"]
+y1 = ["0.1 ms", "1 ms", "10 ms","0.1 s", "1 s", "10 s", "1 min", "10 min"]
+y2 = ["1 KB", "10KB", "100 KB", "1 MB", "10 MB", "100 MB", "1 GB", "10 GB"]
+tiempo1 = [0.1/1000,1/1000,10/1000,0.1,1,10,60,60*10]
+memoria1 = [1*10**3,10*10**3,100*10**3,1*10**6,10*10**6,100*10**6,1*10**9,10*10**9]
+tamano1 = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+
+plt.subplot(2,1,1)
+plt.title("Rendimiento A@B")
+r=0
+t=0
+while r < 10:
+    while t < len(datos):
+        plt.loglog(tamano[t:t+30],tiempo[t:t+30], "o-")
+        t+=N
+    r+=1
+    
+plt.yticks(tiempo1, y1)
+plt.xticks(tamano1, ["","","","","","","","","",""])
+plt.xlim(right=20000)
+plt.grid()
+plt.ylabel("Tiempo transcurrido")
+
+plt.subplot(2,1,2)
+plt.loglog(tamano, memoria,"o-")
+plt.hlines(y=8*10**9, xmin=0, xmax=20000, linestyle="--")
+plt.xticks(tamano1, x1, rotation=45)
+plt.yticks(memoria1,y2)
+plt.xlim(right=20000)
+plt.grid()
+plt.xlabel("Tamaño matriz N")
+plt.ylabel("Uso memoria")
+
+plt.savefig("Redimiento A@B")
+plt.show()
+
+
--- timing_matmul.py
+++ timing_matmul.py
@@ -2,28 +2,44 @@
 from numpy import zeros
 from time import perf_counter
 
-"""Tamaño"""
-N=1000
+i = 0
+tiempo = []
+memoria = []
+tamano = []
+while i < 10:
+    N=[3,7,10,20,50,70,85,100,150,200,250,300,
+       400,500,750,800,950,1000,1250,1400,1500,1650,
+       1850,1950,2000,2400,3300,4600,5000,7500,10000]         #Tamaño de las matrices
+    n = 0
+    while len(N)>n:
+        A = zeros((N[n], N[n]))+1
+        B = zeros((N[n], N[n]))+2
+        
+        t1 = perf_counter()
+        C = A@B
+        t2 = perf_counter()   
+        mem = A.nbytes + B.nbytes + C.nbytes
+        dt = t2-t1
+        if mem >= 5000000000:               #Si la memoria es mayor a 5GB se termina el ciclo
+            break
+        
+        tiempo.append(dt)
+        memoria.append(mem)
+        tamano.append(N[n])
+        
+        print (f"dt = {dt} s")
+        print (f"mem = {mem} bytes")
+        print (f"N = {N[n]}")
+        print ()
+        n+=1
+    i+=1
 
-A = zeros((N, N))+1
-B = zeros((N, N))+2
-
-t1 = perf_counter()
-C = A@B
-t2 = perf_counter()
-
-dt = t2-t1
-
-print (f"dt = {dt} s")
-
-
-
-
-
-
-
-
-
-
+f = open("Rendimiento.txt", "w")
+e = 0
+f.write("Tiempo[s]   Memoria[bytes]    Tamaño \n")
+while e < len(tiempo):
+    f.write(f"{tiempo[e]}   {memoria[e]}   {tamano[e]} \n")
+    e+=1
+f.close()
 
 
